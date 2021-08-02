4a1cc43453c29856182d9a34831ef8b0c8fb73b2
holivieri <hernan.olivieri@gmail.com>
2021-08-02 08:56:27 -0700

    first checkin

---
+++ example/lib/movie_bloc.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'api_response.dart';
+import 'movie_repository.dart';
+import 'movie_response.dart';
+
+class MovieBloc {
+  MovieRepository _movieRepository;
+
+  StreamController _movieListController;
+
+  StreamSink<ApiResponse<List<Movie>>> get movieListSink =>
+      _movieListController.sink;
+
+  Stream<ApiResponse<List<Movie>>> get movieListStream =>
+      _movieListController.stream;
+
+  MovieBloc() {
+    _movieListController = StreamController<ApiResponse<List<Movie>>>();
+    _movieRepository = MovieRepository();
+    fetchMovieList();
+  }
+
+  fetchMovieList() async {
+    movieListSink.add(ApiResponse.loading('Fetching Popular Movies'));
+    try {
+      List<Movie> movies = await _movieRepository.fetchMovieList();
+      movieListSink.add(ApiResponse.completed(movies));
+    } catch (e) {
+      movieListSink.add(ApiResponse.error(e.toString()));
+      print(e);
+    }
+  }
+
+  dispose() {
+    _movieListController?.close();
+  }
+}
